16cdca2328d7d63be2b29a64f6893e9c68339ade
Alberto <amarturelo@gmail.com>
2023-02-21 17:18:49 -0300

    commit init

---
+++ app/src/main/java/ConfigData.kt
@@ -0,0 +1,6 @@
+object ConfigData{
+    const val projectName = "usersgithub-android-followers"
+    const val groupName = "com.amarturelo.usersgithub.android"
+    const val versionName = "1.0.0"
+    const val artifactId = "followers"
+}
