dd7e50fe8f2802b3e09db6ddd0e9b95de07d3a74
Alberto <amarturelo@gmail.com>
2023-02-23 13:14:59 -0300

    fix issues with core dependencies

--- buildSrc/src/main/kotlin/ApplicationModules.kt
+++
@@ -1,3 +0,0 @@
-object ApplicationModules {
-    const val ugCore = "com.amarturelo.usersgithub.android:core:1.0.6-SNAPSHOT"
-}
--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -1,6 +1,6 @@
 object ConfigData{
     const val projectName = "usersgithub-android-followers"
     const val groupName = "com.amarturelo.usersgithub.android"
-    const val versionName = "1.0.1"
+    const val versionName = "1.0.2"
     const val artifactId = "followers"
 }
